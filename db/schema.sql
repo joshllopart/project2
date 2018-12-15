7e71b44edebfd98cf9436ee468d0f01619447113
Justin Kunz <Justin_Kunz@yahoo.com>
2018-12-14 19:54:57 -0600

    added affiliate link logic and redirect link code

--- db/schema.sql
+++ db/schema.sql
@@ -1,25 +1,12 @@
-drop database if exists gift_help_db;
-
-create database gift_help_db;
-
 use gift_help_db;
 
-create table requests(
+create table links(
 id int auto_increment,
-req_email varchar(50),
-req_msg varchar(1000),
-budget int,
-answered boolean default false,
-createdAt timestamp,
-primary key (id)
-);
-
-create table answers(
-id int auto_increment,
-res_msg varchar(500) not null,
-shop_link varchar(500),
+provided_link varchar(500) not null,
+converted_link varchar(500) not null,
 req_id int not null,
 primary key (id),
 createdAt timestamp,
+updatedAt timestamp,
 foreign key (req_id) references requests(id)
 );
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,7 +6,7 @@ module.exports = function (app) {
   //Get call for getting random category
   app.get("/api/get/random/:cat", function (req, res) {
     var queryObj = {}
-    
+
     if (req.params.cat !== "All") {
 
       queryObj = {
@@ -39,10 +39,10 @@ module.exports = function (app) {
           category: er.category,
           id: er.id
         }
-      res.json(returnedObj)
+        res.json(returnedObj)
       }
-      catch(err) {
-         res.json("no results")
+      catch (err) {
+        res.json("no results")
       }
     });
   });
@@ -66,39 +66,73 @@ module.exports = function (app) {
   //Post call for submitting answers
   app.post("/api/post/answer", function (req, res) {
 
-    //create answer row in db
-    db.Answers.create(req.body).then(function (result) {
-      var answerId = result.dataValues.id
+    var affiliate = false
+    var newLink = req.body.shop_link
+    link = newLink.split("/")
 
-      //update request answered to true
-      db.Requests.update({ answered: true }, { where: { id: req.body.req_id } }).then(function (response) {
 
-        //grab request data 
-        db.Requests.hasMany(db.Answers, { foreignKey: 'req_id' })
-        db.Answers.belongsTo(db.Requests, { foreignKey: 'id' })
-        db.Requests.findOne({
-          include: {
-            model: db.Answers,
-            on: {
-              id: answerId
+    //if amazon link create a new link
+    if (link[2] === "www.amazon.com") {
+      dp = link.indexOf("dp")
+      ASIN = link[dp + 1]
+      newLink = "http://www.amazon.com/dp/" + ASIN + "/?tag=gifthelp03-20"
+      affiliate = true
+      console.log(newLink)
+    }
+
+    //post link in db
+    var linkObj = {
+      provided_link: req.body.shop_link,
+      converted_link: newLink,
+      req_id: req.body.req_id,
+      affiliate_link: affiliate,
+      visits: 0
+    }
+
+    db.Links.create(linkObj).then(function (result) {
+      console.log(result.dataValues.id)
+
+      //generate redirect link from db results
+      redirectLink = "http://www.gifthelp.xyz/api/get/redirect/" + result.dataValues.id
+
+      //create answer row in db
+      db.Answers.create({
+        res_msg: req.body.res_msg,
+        req_id: req.body.req_id,
+        shop_link: redirectLink
+      }).then(function (result) {
+        var answerId = result.dataValues.id
+
+        //update request answered to true
+        db.Requests.update({ answered: true }, { where: { id: req.body.req_id } }).then(function (response) {
+
+          //grab request data 
+          db.Requests.hasMany(db.Answers, { foreignKey: 'req_id' })
+          db.Answers.belongsTo(db.Requests, { foreignKey: 'id' })
+          db.Requests.findOne({
+            include: {
+              model: db.Answers,
+              on: {
+                id: answerId
+              }
+            },
+            where: {
+              id: req.body.req_id
             }
-          },
-          where: {
-            id: req.body.req_id
-          }
-        }).then(function (response) {
-          
-          //prepare and send email
-          var emailObj = {
-            email: response.dataValues.req_email,
-            req_msg: response.dataValues.req_msg,
-            res_msg: response.dataValues.Answers[0].res_msg,
-            shop_link: response.dataValues.Answers[0].shop_link,
-            id: response.dataValues.id
-          }
+          }).then(function (response) {
 
-          email.answer(emailObj)
-        });
+            //prepare and send email
+            var emailObj = {
+              email: response.dataValues.req_email,
+              req_msg: response.dataValues.req_msg,
+              res_msg: response.dataValues.Answers[0].res_msg,
+              shop_link: response.dataValues.Answers[0].shop_link,
+              id: response.dataValues.id
+            }
+
+            email.answer(emailObj)
+          });
+        })
       })
     })
   });
@@ -110,4 +144,21 @@ module.exports = function (app) {
     })
   });
 
+
+  //for redirecting email links
+  app.get("/api/get/redirect/:id", function (req, res) {
+
+    //find link parameter
+    db.Links.findOne({ where: { id: req.params.id } }).then(function (result) {
+
+     var redirect = result.dataValues.converted_link
+      //update click count
+      db.Links.update({ visits: result.dataValues.visits + 1 }, { where: { id: req.params.id } }).then(function (result) {
+
+        //redirect to converted link
+        res.redirect(redirect)
+
+      })
+    })
+  })
 }
